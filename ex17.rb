ce6c06dc9815686aeee37a157e6476afc9fbe776
dashsoul92 <dashsoul92@gmail.com>
2017-11-02 18:51:24 -0600

    Completed exercise 18.

--- ex17.rb
+++ ex17.rb
@@ -9,8 +9,6 @@ in_data = in_file.read
 puts "The input file is #{in_data.length} bytes long"
 
 puts "Does the output file exist? #{File.exist?(to_file)}"
-puts "Ready, hit RETURN to continue, CTRL-C to abort."
-$stdin.gets
 
 out_file = open(to_file, 'w')
 out_file.write(in_data)
